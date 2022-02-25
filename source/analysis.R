91004fddb9d148d997fc051a05fb5bfb91495856
Justin Rhinehart <jjrhine@uw.edu>
2022-02-24 17:01:35 -0800

    upading index and analysis with comp and trend charts, w/ writing

--- source/analysis.R
+++ source/analysis.R
@@ -2,30 +2,20 @@ library(dplyr)
 library(ggplot2)
 library(leaflet)
 library(plotly)
+library(tidyverse)
+library(maps)
+library(mapproj)
+library(patchwork)
 
 # data import
 
 veraRawCounty  <- "https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv"
 veraDataCounty <- read.csv(veraRawCounty)
 
-veraRawJurisdiction <- "https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv"
-veraDataJurisdiction <- read.csv(veraRawJurisdiction)
+# veraRawJurisdiction <- "https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv"
+# veraDataJurisdiction <- read.csv(veraRawJurisdiction)
 
-# Trend Chart - US Prison & Jail population by race 
-# Columns to use
-  # year
-  # aapi_jail_pop 
-  # aapi_prison_pop
-  # black_jail_pop
-  # black_prison_pop
-  # latinx_jail_pop
-  # latinx_prison_pop
-  # native_jail_pop
-  # native_prison_pop
-  # white_jail_pop
-  # white_prison_pop
-  # other_race_jail_pop
-  # other_race_prison_pop
+# --------------- Trend Chart ---------------
 
 prisonPopRace <- veraDataCounty %>%
   group_by(year) %>%
@@ -37,37 +27,54 @@ prisonPopRace <- veraDataCounty %>%
   distinct(year, year_aapi_JoP_pop, year_black_JoP_pop, year_latinx_JoP_pop, year_native_JoP_pop, year_white_JoP_pop) %>%
   filter(year >= 1990)
 
-ggplot(prisonPopRace, aes(x=year)) + 
-  geom_line(aes(y = year_aapi_JoP_pop), color = "red") + 
-  geom_line(aes(y = year_black_JoP_pop), color="blue") + 
-  geom_line(aes(y = year_latinx_JoP_pop), color="darkgreen") + 
-  geom_line(aes(y = year_native_JoP_pop), color="orange") + 
-  geom_line(aes(y = year_white_JoP_pop), color="black") + 
-  xlab("Year") + 
-  ylab("Percentage of Population in Prison or Jail")
+incarceratedPopProp <- ggplot(prisonPopRace, aes(x=year)) + 
+  geom_line(aes(y = year_aapi_JoP_pop, color = "Asian American / Pacific Islander")) + 
+  geom_line(aes(y = year_black_JoP_pop, color="Black")) + 
+  geom_line(aes(y = year_latinx_JoP_pop, color="Latinx")) + 
+  geom_line(aes(y = year_native_JoP_pop, color="Native")) + 
+  geom_line(aes(y = year_white_JoP_pop, color="White")) + 
+  labs(title = "Percentage of US Population in Prison or Jail, by Race", 
+       x = "Year", 
+       y = "Percentage of Population",
+       color = "Demographic",
+       subtitle = "1990 to 2018")
 
-# Compare Chart - Total Pop vs Total Prison & Jail Pop over Time
-# Columns to use
-  # year
-  # total_pop
-  # total_jail_pop 
-  # total_prison_pop
+ggplotly(incarceratedPopProp)
+
+# --------------- Compare Chart ---------------
+
+prisonPopGenPop <- veraDataCounty %>%
+  group_by(year) %>%
+  mutate(year_gen_pop = sum(total_pop, na.rm = TRUE),
+         year_prison_pop = sum(total_jail_pop, na.rm = TRUE) + sum(total_prison_pop, na.rm = TRUE)) %>%
+  distinct(year, year_gen_pop, year_prison_pop)
+
+prisonPopGenPopGraph <- ggplot(prisonPopGenPop, aes(x=year_gen_pop)) + 
+  geom_line(aes(y = year_prison_pop)) + 
+  labs(title = "US General Population vs. Incarcerated Population Between 1970 and 2018", 
+       x = "General Population", 
+       y = "Incarcerated Population")
+
+ggplotly(prisonPopGenPopGraph)
+
+# --------------- Map ---------------
+  # Proportion of black incarcerated persons by state
+  # Columns to use
+    # State
+    # black_jail_pop
+    # black_prison_pop
+    # total_jail_pop 
+    # total_prison_pop
 
 
+# --------------- Values ---------------
 
-# Map - White vs. Non-white prison & Jail proportions by State
-# Columns to use
-  # State
-  # aapi_jail_pop 
-  # black_jail_pop
-  # latinx_jail_pop
-  # native_jail_pop
-  # white_jail_pop
-  # other_race_jail_pop
+  # max incarceration rates
+aapiMaxRate <- max(prisonPopRace$year_aapi_JoP_pop)
+blackMaxRate <- max(prisonPopRace$year_black_JoP_pop)
+latinxMaxRate <- max(prisonPopRace$year_latinx_JoP_pop)
+nativeMaxRate <- max(prisonPopRace$year_native_JoP_pop)
+whiteMaxRate <- max(prisonPopRace$year_white_JoP_pop)
 
-                       
-                       
-                       
-                       
-                       
-                       
+  # tells us that for every 59.17 people we add, one of them will go to prison
+rocGenPrisonPop <- 1/((prisonPopGenPop$year_prison_pop[19] - prisonPopGenPop$year_prison_pop[1]) / (prisonPopGenPop$year_gen_pop[19] - prisonPopGenPop$year_gen_pop[1]))
